c8fe6930db47f425949ce0f925a19791029eb92e
Ingrid Biery <ingridbiery@gmail.com>
2023-08-20 21:59:06 -0400

    improve seeds

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,7 +1,7 @@
 class Family < ApplicationRecord
   has_many :people, dependent: :destroy
 
-  LEGAL_CHARS = /\A[a-zA-Z. \-\(\)\/']*\z/
+  LEGAL_CHARS = /\A[0-9a-zA-Z. \-\(\)\/']*\z/
   LEGAL_CHARS_MSG = "contains invalid characters. Valid characters: letters, space, period, dash, parentheses, slash."
 
   validates :name, presence: true,
