34694db213ca6c086eb5411502a446419bd962a1
Ingrid Biery <ingridbiery@gmail.com>
2026-05-22 23:14:07 -0400

    fix arts system tests

--- test/system/arts_test.rb
+++ test/system/arts_test.rb
@@ -2,42 +2,72 @@ require "application_system_test_case"
 
 class ArtsTest < ApplicationSystemTestCase
   setup do
-    @art = arts(:one)
+    @art = create(:art)
   end
 
   test "visiting the index" do
     visit arts_url
+
     assert_selector "h1", text: "Arts"
+    assert_text "#{@art.name} (#{@art.abbrev})"
+    assert_link "Edit Art"
+    assert_link "New art"
   end
 
-  test "should create art" do
+  test "creating an art" do
     visit arts_url
     click_on "New art"
 
-    fill_in "Abbrev", with: @art.abbrev
-    fill_in "Name", with: @art.name
+    art_params = attributes_for(:art)
+
+    fill_in "Name", with: art_params[:name]
+    fill_in "Abbrev", with: art_params[:abbrev]
     click_on "Create Art"
 
-    assert_text "Art was successfully created"
-    click_on "Back"
+    # Your index page shows: "#{name} (#{abbrev})"
+    assert_text "#{art_params[:name]} (#{art_params[:abbrev]})"
+    assert_link "Back to arts"
   end
 
-  test "should update Art" do
+  test "updating an art" do
     visit art_url(@art)
-    click_on "Edit this art", match: :first
 
-    fill_in "Abbrev", with: @art.abbrev
-    fill_in "Name", with: @art.name
+    click_on "Edit", match: :first
+
+    art_params = attributes_for(:art)
+
+    fill_in "Name", with: art_params[:name]
+    fill_in "Abbrev", with: art_params[:abbrev]
     click_on "Update Art"
 
-    assert_text "Art was successfully updated"
-    click_on "Back"
+    assert_text "#{art_params[:name]} (#{art_params[:abbrev]})"
+    assert_link "Back to arts"
   end
 
-  test "should destroy Art" do
+  test "destroying an art" do
     visit art_url(@art)
-    click_on "Destroy this art", match: :first
 
-    assert_text "Art was successfully destroyed"
+    # Your show page uses button_to "Destroy this art"
+    accept_confirm do
+      click_on "Destroy this art"
+    end
+
+    assert_selector "h1", text: "Arts"
+    assert_no_text "#{@art.name} (#{@art.abbrev})"
+  end
+
+  test "show page renders nested belts and courses sections" do
+    course = create(:course, art: @art)
+    belt   = create(:belt, art: @art)
+
+    visit art_url(@art)
+
+    # From _art.html.erb
+    assert_text "Courses (#{@art.courses.count})"
+    assert_text "Belts (#{@art.belts.count})"
+
+    # Ensure nested tables render
+    assert_text course.day if course.day.present?
+    assert_text belt.level if belt.level.present?
   end
 end
