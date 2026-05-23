d726514b823d2d163159b81cc817b892c19881cb
Ingrid Biery <ingridbiery@gmail.com>
2026-05-22 23:38:45 -0400

    fix courses tests

--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -2,44 +2,82 @@ require "application_system_test_case"
 
 class CoursesTest < ApplicationSystemTestCase
   setup do
-    @course = courses(:one)
+    @art = create(:art)
+    @course = create(:course, art: @art)
   end
 
   test "visiting the index" do
-    visit courses_url
+    visit art_courses_path(@art)
+
     assert_selector "h1", text: "Courses"
+    assert_text @course.day
+    assert_text @course.time.strftime("%I:%M %p")
+    assert_link "Add"
+    assert_link "Show this course"
   end
 
-  test "should create course" do
-    visit courses_url
-    click_on "New course"
+  test "creating a course" do
+    visit art_courses_path(@art)
 
-    fill_in "Art", with: @course.art_id
-    fill_in "Date", with: @course.date
-    fill_in "Time", with: @course.time
+    within("#add-course") do
+      click_on "Add"
+    end
+
+    assert_selector "h1", text: "New course"
+    assert_text @art.name
+
+    course_params = attributes_for(:course)
+
+    select course_params[:day], from: "Day"
+    fill_in "Time", with: course_params[:time]
+
     click_on "Create Course"
 
-    assert_text "Course was successfully created"
-    click_on "Back"
+    assert_text course_params[:day]
+    assert_text course_params[:time].strftime("%I:%M %p")
   end
 
-  test "should update Course" do
-    visit course_url(@course)
-    click_on "Edit this course", match: :first
+  test "editing a course" do
+    visit art_course_path(@art, @course)
+    click_on "Edit this course"
 
-    fill_in "Art", with: @course.art_id
-    fill_in "Date", with: @course.date
-    fill_in "Time", with: @course.time
+    assert_selector "h1", text: "Editing course"
+
+    course_params = attributes_for(:course)
+
+    select course_params[:day], from: "Day"
+    fill_in "Time", with: course_params[:time].strftime("%I:%M %p")
+
     click_on "Update Course"
 
-    assert_text "Course was successfully updated"
-    click_on "Back"
+    assert_text course_params[:day]
   end
 
-  test "should destroy Course" do
-    visit course_url(@course)
-    click_on "Destroy this course", match: :first
+  test "show page displays meetings count" do
+    meeting = create(:meeting, course: @course)
 
-    assert_text "Course was successfully destroyed"
+    visit art_course_path(@art, @course)
+
+    assert_selector "h3", text: "Meetings (1)"
+    assert_text meeting.date.to_s
+  end
+
+  test "index page shows meetings link" do
+    visit art_courses_path(@art)
+
+    within("#courses") do
+      assert_link "New", href: new_art_course_meeting_path(@art, @course)
+    end
+  end
+
+  test "destroying a course" do
+    visit art_course_path(@art, @course)
+
+    accept_confirm do
+      click_on "Destroy this course"
+    end
+
+    assert_current_path art_path(@art)
+    assert_no_text @course.day
   end
 end
