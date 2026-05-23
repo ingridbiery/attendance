0308c8dcc453ebd31300563295a374f6a04b3331
Ingrid Biery <ingridbiery@gmail.com>
2026-05-23 11:32:23 -0400

    more improvements to arts system tests

--- test/system/arts_test.rb
+++ test/system/arts_test.rb
@@ -1,3 +1,4 @@
+# test/system/arts_test.rb
 require "application_system_test_case"
 
 class ArtsTest < ApplicationSystemTestCase
@@ -8,15 +9,19 @@ class ArtsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit arts_url
 
+    assert_current_path arts_path
     assert_selector "h1", text: "Arts"
-    assert_text "#{@art.name} (#{@art.abbrev})"
-    assert_link "Edit Art"
-    assert_link "New art"
+    assert_link "#{@art.name} (#{@art.abbrev})", href: art_path(@art)
+    assert_link "Edit Art", href: edit_art_path(@art)
+    assert_link "New art", href: new_art_path
   end
 
   test "creating an art" do
-    visit arts_url
-    click_on "New art"
+    visit new_art_url
+
+    assert_current_path new_art_path
+    assert_selector "h1", text: "New art"
+    assert_link "Back to arts", href: arts_path
 
     art_params = attributes_for(:art)
 
@@ -24,15 +29,27 @@ class ArtsTest < ApplicationSystemTestCase
     fill_in "Abbrev", with: art_params[:abbrev]
     click_on "Create Art"
 
-    # Your index page shows: "#{name} (#{abbrev})"
-    assert_text "#{art_params[:name]} (#{art_params[:abbrev]})"
-    assert_link "Back to arts"
+    # put these first so we wait for the page to load
+    assert_selector "h2", text: "#{art_params[:name]} (#{art_params[:abbrev]})"
+    assert_selector "h3", text: "Courses (0)"
+    assert_selector "h3", text: "Belts (0)"
+    puts current_path
+    new_art_id = current_path.split("/").last.to_i
+    new_art = Art.find(new_art_id)
+    assert_current_path art_path(new_art)
+    assert_link "Add", href: new_art_course_path(new_art)
+    assert_link "Add", href: new_art_belt_path(new_art)
+    assert_link "Edit", href: edit_art_path(new_art)
+    assert_link "Back to arts", href: arts_path
   end
 
-  test "updating an art" do
-    visit art_url(@art)
+  test "editing an art" do
+    visit edit_art_url(@art)
 
-    click_on "Edit", match: :first
+    assert_current_path edit_art_path(@art)
+    assert_selector "h1", text: "Editing art"
+    assert_link "Show this art", href: art_path(@art)
+    assert_link "Back to arts", href: arts_path
 
     art_params = attributes_for(:art)
 
@@ -40,34 +57,60 @@ class ArtsTest < ApplicationSystemTestCase
     fill_in "Abbrev", with: art_params[:abbrev]
     click_on "Update Art"
 
-    assert_text "#{art_params[:name]} (#{art_params[:abbrev]})"
-    assert_link "Back to arts"
+    assert_current_path art_path(@art)
+    assert_selector "h2", text: "#{art_params[:name]} (#{art_params[:abbrev]})"
+    assert_link "Edit", href: edit_art_path(@art)
+    assert_link "Back to arts", href: arts_path
+  end
+
+  test "show page renders art details" do
+    visit art_url(@art)
+
+    assert_current_path art_path(@art)
+    assert_selector "h2", text: "#{@art.name} (#{@art.abbrev})"
+    assert_selector "h3", text: "Courses (0)"
+    assert_selector "h3", text: "Belts (0)"
+    assert_link "Add", href: new_art_course_path(@art)
+    assert_link "Add", href: new_art_belt_path(@art)
+    assert_link "Edit", href: edit_art_path(@art)
+    assert_link "Back to arts", href: arts_path
+  end
+
+  test "show page renders nested courses" do
+    course = create(:course, art: @art)
+
+    visit art_url(@art)
+
+    assert_current_path art_path(@art)
+    assert_selector "h3", text: "Courses (1)"
+    assert_text course.day
+    assert_text course.time.strftime("%I:%M %p")
+    assert_link "Show", href: art_course_path(@art, course)
+    assert_link "Edit", href: edit_art_course_path(@art, course)
+  end
+
+  test "show page renders nested belts" do
+    belt = create(:belt, art: @art)
+
+    visit art_url(@art)
+
+    assert_current_path art_path(@art)
+    assert_selector "h3", text: "Belts (1)"
+    assert_link belt.level, href: art_belt_path(@art, belt)
+    assert_text belt.rank.to_s
+    assert_link "Show", href: art_belt_path(@art, belt)
+    assert_link "Edit", href: edit_art_belt_path(@art, belt)
   end
 
   test "destroying an art" do
     visit art_url(@art)
 
-    # Your show page uses button_to "Destroy this art"
     accept_confirm do
       click_on "Destroy this art"
     end
 
+    assert_current_path arts_path
     assert_selector "h1", text: "Arts"
     assert_no_text "#{@art.name} (#{@art.abbrev})"
-  end
-
-  test "show page renders nested belts and courses sections" do
-    course = create(:course, art: @art)
-    belt   = create(:belt, art: @art)
-
-    visit art_url(@art)
-
-    # From _art.html.erb
-    assert_text "Courses (#{@art.courses.count})"
-    assert_text "Belts (#{@art.belts.count})"
-
-    # Ensure nested tables render
-    assert_text course.day if course.day.present?
-    assert_text belt.level if belt.level.present?
   end
 end
