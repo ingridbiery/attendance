3b6d36f57d69fb5524582ae7c1c7d910c5d9d0aa
Ingrid Biery <ingridbiery@gmail.com>
2026-05-23 11:53:43 -0400

    more improvements to meetings system tests

--- test/system/meetings_test.rb
+++ test/system/meetings_test.rb
@@ -1,3 +1,4 @@
+# test/system/meetings_test.rb
 require "application_system_test_case"
 
 class MeetingsTest < ApplicationSystemTestCase
@@ -8,25 +9,33 @@ class MeetingsTest < ApplicationSystemTestCase
   end
 
   test "creating a meeting" do
-    visit art_courses_url(@art, @course)
+    visit art_courses_url(@art)
 
     click_link "New", href: new_art_course_meeting_path(@art, @course)
 
+    assert_current_path new_art_course_meeting_path(@art, @course)
     assert_selector "h1", text: "New meeting for #{@art.abbrev} #{@course.day}"
+    assert_link "Back to course", href: art_course_path(@art, @course)
 
     meeting_params = attributes_for(:meeting)
 
-    fill_in "Date", with: meeting_params[:date]
+    find("#meeting_date").send_keys(meeting_params[:date].strftime("%m%d%Y"))
     click_on "Create Meeting"
 
-    assert_text meeting_params[:date]
+    # put these first so we wait for the page to load before finding the id
+    assert_selector "h2", text: meeting_params[:date].to_s
+    new_meeting_id = current_path.split("/").last.to_i
+    new_meeting = Meeting.find(new_meeting_id)
+    assert_current_path art_course_meeting_path(@art, @course, new_meeting)
+    assert_link "Back to course", href: art_course_path(@art, @course)
   end
 
-  test "show page displays meeting date" do
+  test "show page renders meeting details" do
     visit art_course_meeting_url(@art, @course, @meeting)
 
+    assert_current_path art_course_meeting_path(@art, @course, @meeting)
     assert_selector "h2", text: @meeting.date.to_s
-    assert_link "Back to course"
+    assert_link "Back to course", href: art_course_path(@art, @course)
   end
 
   test "destroying a meeting" do
@@ -38,5 +47,6 @@ class MeetingsTest < ApplicationSystemTestCase
 
     assert_current_path art_course_path(@art, @course)
     assert_selector "h3", text: "Meetings (0)"
+    assert_no_text @meeting.date.to_s
   end
 end
