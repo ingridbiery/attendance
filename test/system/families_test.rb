f4350f34e80307998c77729b76ab3889b0d946da
Ingrid Biery <ingridbiery@gmail.com>
2026-05-22 23:57:56 -0400

    fixed families system tests

--- test/system/families_test.rb
+++ test/system/families_test.rb
@@ -2,40 +2,62 @@ require "application_system_test_case"
 
 class FamiliesTest < ApplicationSystemTestCase
   setup do
-    @family = families(:one)
+    @family = create(:family)
   end
 
   test "visiting the index" do
     visit families_url
+
     assert_selector "h1", text: "Families"
+    assert_text @family.name
+    assert_link "Edit"
+    assert_link "New family"
   end
 
-  test "should create family" do
+  test "creating a family" do
     visit families_url
     click_on "New family"
 
-    fill_in "Name", with: @family.name
+    assert_selector "h1", text: "New family"
+
+    family_params = attributes_for(:family)
+    fill_in "Name", with: family_params[:name]
     click_on "Create Family"
 
-    assert_text "Family was successfully created"
-    click_on "Back"
+    assert_text family_params[:name]
   end
 
-  test "should update Family" do
-    visit family_url(@family)
-    click_on "Edit this family", match: :first
+  test "editing a family" do
+    visit families_url
+    click_on "Edit", match: :first
 
-    fill_in "Name", with: @family.name
+    assert_selector "h1", text: "Editing family"
+
+    family_params = attributes_for(:family)
+    fill_in "Name", with: family_params[:name]
     click_on "Update Family"
 
-    assert_text "Family was successfully updated"
-    click_on "Back"
+    assert_text family_params[:name]
   end
 
-  test "should destroy Family" do
+  test "destroying a family" do
     visit family_url(@family)
-    click_on "Destroy this family", match: :first
 
-    assert_text "Family was successfully destroyed"
+    accept_confirm do
+      click_on "Destroy this family"
+    end
+
+    assert_current_path families_path
+    within("table") do
+      assert_no_text @family.name
+    end
+  end
+
+  test "show page renders family details" do
+    visit family_url(@family)
+
+    assert_text @family.name
+    assert_link "Edit"
+    assert_link "Back to families"
   end
 end
