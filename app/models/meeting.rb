ba44eddff511bea96501ea39be1c98936723a6af
Ingrid Biery <ingridbiery@gmail.com>
2026-05-25 09:17:59 -0400

    disallow null date on meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,6 +2,9 @@ class Meeting < ApplicationRecord
   belongs_to :course
   has_one :art, through: :course
   has_many :attendances, dependent: :destroy
+  has_many :people, through: :attendances
+
+  validates :date, presence: true
 
   self.implicit_order_column = 'date'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_23_163233) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_25_131613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_23_163233) do
   end
 
   create_table "meetings", force: :cascade do |t|
-    t.date "date"
+    t.date "date", null: false
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
